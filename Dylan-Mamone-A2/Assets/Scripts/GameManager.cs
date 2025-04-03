7b06dc9a34367ae8d3eedabbf55bfc5cfede4276
ChadDylan22 <000915133@mohawkcollege.ca>
2025-04-02 22:47:46 -0400

    Added unity project and git ignore

---
+++ Dylan-Mamone-A2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+}
