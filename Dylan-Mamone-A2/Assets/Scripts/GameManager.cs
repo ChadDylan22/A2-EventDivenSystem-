596a2dc6aded0ecd2ba253c7109cb077c6cc183a
ChadDylan22 <000915133@mohawkcollege.ca>
2025-04-09 15:07:48 -0400

    Added game over function
    -Added game over function to stop gameplay when player collides with an obstacle.

--- Dylan-Mamone-A2/Assets/Scripts/GameManager.cs
+++ Dylan-Mamone-A2/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class GameManager : MonoBehaviour
     public float initialGameSpeed = 5f;
     public float gameSpeedIncrease = 0.1f;
     public float gameSpeed { get; private set; }
+
+    private Player player;
+    private Spawner spawner;
 
     private void Awake()
     {
@@ -29,12 +33,35 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+
         NewGame();
     }
 
     private void NewGame() 
     {
+        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
+
+        foreach (var obstacle in obstacles)
+        {
+            Destroy(obstacle);
+        }
+        
         gameSpeed = initialGameSpeed;
+        enabled = true;
+
+        player.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(true);
+    }
+
+    public void GameOver() 
+    {
+        gameSpeed = 0f;
+        enabled = false;
+
+        player.gameObject.SetActive(false);
+        spawner.gameObject.SetActive(false);
     }
 
     private void Update()
