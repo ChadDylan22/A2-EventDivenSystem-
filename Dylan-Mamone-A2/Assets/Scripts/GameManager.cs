dd577cfb034fce759d48c7825044874e2c16a4b8
ChadDylan22 <000915133@mohawkcollege.ca>
2025-04-09 16:30:33 -0400

    Added functionality to Hi Score
    -Added to Game Manager script to allow the Hi Score to track and stay from game to game.

--- Dylan-Mamone-A2/Assets/Scripts/GameManager.cs
+++ Dylan-Mamone-A2/Assets/Scripts/GameManager.cs
@@ -56,6 +56,7 @@ public class GameManager : MonoBehaviour
             Destroy(obstacle.gameObject);
         }
 
+        score = 0f;
         gameSpeed = initialGameSpeed;
         enabled = true;
 
@@ -63,6 +64,8 @@ public class GameManager : MonoBehaviour
         spawner.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(false);
         retryButton.gameObject.SetActive(false);
+
+        UpdateHiscore();
     }
 
     public void GameOver() 
@@ -74,6 +77,8 @@ public class GameManager : MonoBehaviour
         spawner.gameObject.SetActive(false);
         gameOverText.gameObject.SetActive(true);
         retryButton.gameObject.SetActive(true);
+
+        UpdateHiscore();
     }
 
     private void Update()
@@ -81,5 +86,18 @@ public class GameManager : MonoBehaviour
         gameSpeed += gameSpeedIncrease * Time.deltaTime;
         score += gameSpeed * Time.deltaTime;
         scoreText.text = Mathf.FloorToInt(score).ToString("D5");
+    }
+
+    private void UpdateHiscore() 
+    {
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+
+        if (score > hiscore) 
+        {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore", hiscore);
+        }
+
+        hiscoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
     }
 }
